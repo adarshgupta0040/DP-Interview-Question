50cd1426725a8ecd6eea99d20de2037e045fc2bb
Adarsh Gupta <73924145+adarshgupta0040@users.noreply.github.com>
2022-08-23 01:06:55 +0530

    Update Distinct Subsequences.py

--- Distinct Subsequences.py
+++ Distinct Subsequences.py
@@ -38,5 +38,25 @@ class Solution:
                 else:
                     dp[i][j]= dp[i-1][j]
         return dp[m][n]
+
+# Space Optimisation :
+
+    def numDistinct(self, s: str, t: str) -> int:
+        m=len(s)
+        n=len(t)
+        # dp=[[-1 for i in range(n+1)]for j in range(m+1)]
+        # return self.f(m,n,s,t,dp)
+        prev=[0]*(n+1)
+        prev[0]=1
+        for i in range(1,m+1):
+            curr=[0]*(n+1)
+            curr[0]=1
+            for j in range(1,n+1):
+                if s[i-1]==t[j-1]:
+                    curr[j]= prev[j-1]+prev[j]
+                else:
+                    curr[j]= prev[j]
+            prev=curr
+        return prev[n]
 	
 	
