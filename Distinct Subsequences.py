8ba91f4d1a064d4a63b4d5980e94afb0030e6d4b
Adarsh Gupta <73924145+adarshgupta0040@users.noreply.github.com>
2022-08-23 01:17:15 +0530

    Update Distinct Subsequences.py

--- Distinct Subsequences.py
+++ Distinct Subsequences.py
@@ -59,4 +59,15 @@ class Solution:
             prev=curr
         return prev[n]
 	
-	
+# Single Array 
+
+    def numDistinct(self, s: str, t: str) -> int:
+        m=len(s)
+        n=len(t)
+        prev=[0]*(n+1)
+        prev[0]=1
+        for i in range(1,m+1):
+            for j in range(n,0,-1):
+                if s[i-1]==t[j-1]:
+                    prev[j]= prev[j-1]+prev[j]
+        return prev[n]
