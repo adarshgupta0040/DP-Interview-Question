fe84734d66e0e0e9cbdacc6746348db0c938f497
Adarsh Gupta <73924145+adarshgupta0040@users.noreply.github.com>
2022-08-23 22:34:34 +0530

    Update Edit Distance.py

--- Edit Distance.py
+++ Edit Distance.py
@@ -80,3 +80,21 @@ class Solution:
                 else:
                     dp[i][j]= 1 + min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
         return dp[m][n]
+
+# Space Optimised :
+def editDistance(word1, word2) :
+        m=len(word1)
+        n=len(word2)
+        prev=[0]*(n+1)
+        for j in range(n+1):
+            prev[j]=j
+        for i in range(1,m+1):
+            curr=[0]*(n+1)
+            curr[0]=i
+            for j in range(1,n+1):
+                if word1[i-1]==word2[j-1]:
+                    curr[j]=prev[j-1]
+                else:
+                    curr[j]= 1 + min(prev[j],min(curr[j-1],prev[j-1]))
+            prev=curr
+        return curr[-1]
